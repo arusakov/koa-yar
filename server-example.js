072237c66dde9369db65158fd53f4101a9831bfe
arusakov <a_s_rusakov@mail.ru>
2017-07-23 23:49:48 +0300

    get, post, put and simple routing

--- server-example.js
+++ server-example.js
@@ -1,13 +1,19 @@
 const Koa = require('koa')
 const { Router } = require('.')
+// const Router = require('koa-router')
 
 const app = new Koa()
 const router = new Router()
 
-app.use(router.routes())
-app.use(async ctx => {
-  ctx.body = await Promise.resolve('Hello Koa')
+router.get('/1', (ctx) => {
+  ctx.body = '/1' 
 })
+
+router.get('/2', (ctx) => {
+  ctx.body = '/2' 
+})
+
+app.use(router.routes())
 
 app.listen(3000, () => {
   console.log('Server running on 3000 port...')
